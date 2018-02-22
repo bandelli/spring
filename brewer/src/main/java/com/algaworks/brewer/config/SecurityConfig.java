9d5ca28f98c819811e0d503f657ab73b1f03950d
Mari Martin <bandelli@gmail.com>
2018-02-22 15:45:38 -0300

    Segurança nas telas e correçao de caracteres especiais

--- brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.algaworks.brewer.security.AppUserDetailsService;
 
@@ -44,6 +45,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 				.loginPage("/login")
 				.permitAll()
+				.and()
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 				.and()
 			.exceptionHandling()
 				.accessDeniedPage("/403")
